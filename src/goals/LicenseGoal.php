b2ae004d699f2c5694b0a79ba8d4fab783afb6ea
Andrii Vasyliev <sol@hiqdev.com>
2015-11-24 15:35:37 +0000

    added tests and Travis CI

--- src/goals/LicenseGoal.php
+++ src/goals/LicenseGoal.php
@@ -1,12 +1,12 @@
 <?php
 
 /*
- * Task runner, code generator and build tool for easier continuos integration
+ * License plugin for HiDev
  *
- * @link      https://github.com/hiqdev/hidev
- * @package   hidev
+ * @link      https://github.com/hiqdev/hidev-license
+ * @package   hidev-license
  * @license   BSD-3-Clause
- * @copyright Copyright (c) 2014-2015, HiQDev (http://hiqdev.com/)
+ * @copyright Copyright (c) 2015, HiQDev (http://hiqdev.com/)
  */
 
 namespace hidev\license\goals;
--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -17,4 +17,4 @@ $_SERVER['SCRIPT_NAME']     = '/' . __DIR__;
 $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
-Yii::setAlias('@hidev\codeception', dirname(__DIR__));
+Yii::setAlias('@hidev\license', dirname(__DIR__));
