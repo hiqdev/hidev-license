73ab0616e226ae6600f253c912bb5edc325a146b
Andrii Vasyliev <sol@hiqdev.com>
2017-04-30 13:06:10 +0200

    csfixed

--- src/config/hidev.php
+++ src/config/hidev.php
@@ -1,12 +1,11 @@
 <?php
-
-/*
+/**
  * License plugin for HiDev
  *
  * @link      https://github.com/hiqdev/hidev-license
  * @package   hidev-license
  * @license   BSD-3-Clause
- * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ * @copyright Copyright (c) 2015-2017, HiQDev (http://hiqdev.com/)
  */
 
 return [
