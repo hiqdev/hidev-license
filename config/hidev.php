ee38e28ce520f96b0f7b83da4f052bedcd061e93
Andrii Vasyliev <sol@hiqdev.com>
2020-07-10 13:40:22 +0000

    Use real path for views instead of alias

--- config/hidev.php
+++ config/hidev.php
@@ -21,7 +21,7 @@ return [
         'view' => [
             'theme' => [
                 'pathMap' => [
-                    '@hidev/views' => ['@hidev/license/views'],
+                    '@hidev/views' => [dirname(__DIR__) . '/src/views'],
                 ],
             ],
         ],
